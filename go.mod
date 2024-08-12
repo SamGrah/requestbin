995f02e2747602c1151efec7d7e00152d38acd41
SamGrah <segrah@gmail.com>
2024-08-12 00:42:27 -0400

    added sqlite3 & db

--- go.mod
+++ go.mod
@@ -8,3 +8,5 @@ require (
 )
 
 require github.com/a-h/templ v0.2.747
+
+require github.com/mattn/go-sqlite3 v1.14.22
--- internal/db/db.go
+++ internal/db/db.go
@@ -1,11 +1,25 @@
 package db
 
-type Db struct{}
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Db struct {
+	conn *sql.DB
+}
 
 func NewDb() Db {
 	return Db{}
 }
 
 func (db *Db) Connect() error {
+	conn, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		return err
+	}
+
+	db.conn = conn
 	return nil
 }
